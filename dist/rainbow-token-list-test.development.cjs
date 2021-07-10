1f21b6132067f03a4e7b8bcfc1e8c8b1ec2f6d1a
ctjlewis <ctjlewis@users.noreply.github.com>
2021-07-10 18:25:50 +0000

    auto: refresh Token List from sources, overrides

--- dist/rainbow-token-list-test.development.cjs
+++ dist/rainbow-token-list-test.development.cjs
@@ -12,7 +12,7 @@ function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'defau
 var fetch__default = /*#__PURE__*/_interopDefaultLegacy(fetch);
 
 var name = "Rainbow Token List";
-var timestamp = "2021-07-10T17:39:17.308Z";
+var timestamp = "2021-07-10T18:25:33.383Z";
 var logoURI = "https://avatars0.githubusercontent.com/u/48327834?s=200&v=4";
 var version = {
 	major: 1,
