75dedad9b7d583393c3853777569239609c33611
ctjlewis <ctjlewis@users.noreply.github.com>
2021-08-13 05:18:41 +0000

    auto: refresh Token List from sources, overrides

--- dist/rainbow-token-list-test.development.cjs
+++ dist/rainbow-token-list-test.development.cjs
@@ -12,7 +12,7 @@ function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'defau
 var fetch__default = /*#__PURE__*/_interopDefaultLegacy(fetch);
 
 var name = "Rainbow Token List";
-var timestamp = "2021-08-13T04:27:29.222Z";
+var timestamp = "2021-08-13T05:18:20.508Z";
 var logoURI = "https://avatars0.githubusercontent.com/u/48327834?s=200&v=4";
 var version = {
 	major: 1,
@@ -8251,6 +8251,13 @@ var tokens = [
 		extensions: {
 			isVerified: true
 		}
+	},
+	{
+		address: "0x368BF9F1A1CA767935E39F20439D9041707E2634",
+		chainId: 1,
+		decimals: 18,
+		name: "Chainflix",
+		symbol: "CFXT"
 	},
 	{
 		address: "0x2858eD0225e9aA0638CCd9B552bEE2712426FA24",
